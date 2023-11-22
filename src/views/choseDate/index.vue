54815fde6860a4a3bc258dfe5f28448600e7a6f6
xiaohu <1500980399@qq.com>
2023-11-22 15:48:35 +0800

    add chooseCity component

--- src/views/choseDate/index.vue
+++ src/views/choseDate/index.vue
@@ -1,7 +1,22 @@
 <template>
-  <m-choose-date></m-choose-date>
+  <m-choose-date
+    @startChange="startChange"
+    @endChange="endChange"
+  ></m-choose-date>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+interface dateEndVal {
+  startDate: Date;
+  endDate: Date;
+}
+const startChange = (val: Date) => {
+  console.log(val);
+};
+
+const endChange = (val: dateEndVal) => {
+  console.log(val);
+};
+</script>
 
 <style lang="scss" scoped></style>
